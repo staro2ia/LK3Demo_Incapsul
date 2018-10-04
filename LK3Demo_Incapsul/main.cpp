9003e8494cf74c4f6a8c257aa6c58454992249e6
staro2ia <pasha.st@gmail.com>
2018-10-04 18:51:01 +1000

    Initial Commit

---
+++ LK3Demo_Incapsul/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LK3Demo_Incapsul
+//
+//  Created by Павел Тимош on 04/10/2018.
+//  Copyright © 2018 Павел Тимош. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
