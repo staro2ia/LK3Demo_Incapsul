4135b714ca28f5281f45296b989071ab33073126
staro2ia <pasha.st@gmail.com>
2018-10-04 19:07:15 +1000

    Добавлена заготовка класса лампа.

---
+++ LK3Demo_Incapsul/lamp.cpp
@@ -0,0 +1,9 @@
+//
+//  lamp.cpp
+//  LK3Demo_Incapsul
+//
+//  Created by Павел Тимош on 04/10/2018.
+//  Copyright © 2018 Павел Тимош. All rights reserved.
+//
+
+#include "lamp.hpp"
---
+++ LK3Demo_Incapsul/lamp.hpp
@@ -0,0 +1,31 @@
+//
+//  lamp.hpp
+//  LK3Demo_Incapsul
+//
+//  Created by Павел Тимош on 04/10/2018.
+//  Copyright © 2018 Павел Тимош. All rights reserved.
+//
+
+#ifndef lamp_hpp
+#define lamp_hpp
+
+
+class Lamp {
+public:
+    Lamp();
+    Lamp(bool state, float brightness, int temperature);
+    Lamp(const Lamp& lamp);
+    
+    Lamp operator= (const Lamp& lamp);
+    
+    bool state() const;
+    float brightness() const;
+    int temperature() const;
+    
+private:
+    bool state_;
+    float brightness_;
+    int temperature_;
+};
+
+#endif /* lamp_hpp */
